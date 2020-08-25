c393ed87a3f550473e525d578be135de289ee55e
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2020-08-25 17:56:37 -0400

    changed something

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'What up' });
+  res.send({ hi: 'Tu eres increible!' });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
