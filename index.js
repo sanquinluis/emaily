2ab61142f8dde835abb941eca1d8f0ba4eb08366
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2020-08-25 15:18:52 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'What up' });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
