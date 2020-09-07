c27b2e1e78ecc0ddf5af6623074d02938d704631
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2020-09-07 17:34:43 -0400

    adding client part of project

---
+++ client/src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Landing from './Landing';
+import Header from './Header';
+
+const DashBoard = () => <h2>DashBoard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={DashBoard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Loading...';
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return [
+          <li>
+            <Payments />
+          </li>,
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>,
+        ];
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
