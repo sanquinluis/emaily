8309eb2187993b8edfa9ed67066b1b8ecd8486e9
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2020-09-27 16:39:00 -0400

    testing sendGrid

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,10 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
-
+import axios from 'axios';
+window.axios = axios;
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
